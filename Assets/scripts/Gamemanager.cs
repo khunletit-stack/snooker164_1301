3cbdd81f009aad1f0bcb099e436ba2d09b86d59c
Jewelrypraline <khunletit@gmail.com>
2026-07-25 11:54:31 +0700

    snooker_hopeless

--- Assets/scripts/Gamemanager.cs
+++ Assets/scripts/Gamemanager.cs
@@ -5,7 +5,17 @@ public class Gamemanager : MonoBehaviour
     [SerializeField]
     private int playerscore;
     public int Playerscore { get { return playerscore; } set { playerscore = value; } }
+    
+
+    [SerializeField]
+    private GameObject[] ballposition;
+
+    [SerializeField] 
+    private GameObject ballPrefab; 
+
     public static Gamemanager instance;
+
+
     void Awake()
     {
         instance = this;
@@ -20,5 +30,15 @@ public class Gamemanager : MonoBehaviour
     void Update()
     {
         
+    }
+
+     private void SetBall(Ballcolor col, int i)
+    {
+         GameObject obj  = Instantiate(ballPrefab,
+             ballposition[i].transform.position,
+             Quaternion.identity);
+        ball b = obj.GetComponent<ball>();
+        b.Setcolorandpoint(col);
+
     }
 }
--- Assets/scripts/ball.cs
+++ Assets/scripts/ball.cs
@@ -20,11 +20,16 @@ public class ball : MonoBehaviour, IPointerClickHandler
     [SerializeField]
     private Ballcolor color;
 
+    [SerializeField]
+    private MeshRenderer rb;
+
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log(point);
         Gamemanager.instance.Playerscore += point;
         Destroy(gameObject);
+    }
+    
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -37,5 +42,43 @@ public class ball : MonoBehaviour, IPointerClickHandler
     void Update()
     {
         
+    }
+    public void Setcolorandpoint(Ballcolor col)
+    {
+        switch (col)
+        {
+            case Ballcolor.White:
+                point = 0;
+                rb.material.color = Color.white;
+                break;
+            case Ballcolor.Red:
+                point = 1;
+                rb.material.color = Color.Red;
+                break;
+            case Ballcolor.Yellow:
+                point = 2;
+                rb.material.color = Color.yellow;
+                break;
+            case Ballcolor.Green:
+                point = 3;
+                rb.material.color = Color.green;
+                break;
+            case Ballcolor.Brown:
+                point = 4;
+                rb.material.color = Color.brown;
+                break;
+            case Ballcolor.Blue:
+                point = 5;
+                rb.material.color = Color.blue;
+                break;
+            case Ballcolor.Pink:
+                point = 6;
+                rb.material.color = Color.pink;
+                break;
+            case Ballcolor.Black:
+                point = 7;
+                rb.material.color = Color.black ;
+                break;
+        }
     }
 }
